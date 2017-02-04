acb8844b1408ca07bb23b3f6a945e093e3eedad0
Jonathan De Leon <jonathan@campgroundautomation.com>
2017-02-04 13:33:21 -0800

    Removed vertical drilldown nav bar menu for smaller devices.

--- static/js/app.js
+++ static/js/app.js
@@ -27,7 +27,6 @@ $(document).ready(function() {
         var pageOffsetY = window.pageYOffset;
         var vh = $(window).height();
         if((vh-50) <= pageOffsetY){
-            console.log("here");
             $('#main-menu').css({"position":"fixed", "top":"0","left":"0", "bottom":"0"});
         }else{
             $('#main-menu').css({"position":"absolute", "top":"","left":"","bottom":"0"});
