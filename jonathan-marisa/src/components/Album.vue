b420568ad1955bd628b0b8ce63b4e3f39f93de54
Jonathan De Leon <j.deleon100@yahoo.com>
2018-01-03 10:17:50 -0800

    Using beforeRouteEnter as a navigation guard.

--- jonathan-marisa/src/components/Album.vue
+++ jonathan-marisa/src/components/Album.vue
@@ -1,10 +1,39 @@
 <template>
-  <div id="album">
+  <div id="albums">
     <dashboard :cover-title="title" :background="background"/>
-    <v-container grid-list-sm>
+    <v-container grid-list-md>
+      <v-layout row>
+        <v-toolbar color="transparent" flat>
+          <v-spacer></v-spacer>
+          <v-btn color="primary">Add Photos</v-btn>
+          <v-btn color="success">New Album</v-btn>
+        </v-toolbar>
+      </v-layout>
       <v-layout row wrap>
-        <v-flex xs3 v-for="album in albums" :key="album.id">
-          <photo :url="getThumbnail(album)"/>
+        <v-flex xs10 offset-xs1 sm6 offset-sm0 md4>
+          <photo :url="'/static/media/img/bg2.jpg'">
+            <v-layout media column slot="card-media">
+              <v-spacer></v-spacer>
+              <v-card-title class="headline white--text">All Photos</v-card-title>
+            </v-layout>
+            <v-layout row slot="card-actions">
+              <v-spacer></v-spacer>
+              <v-btn outline color="pink accent-1" :to="'/photos/'">View Album</v-btn>
+            </v-layout>
+          </photo>
+        </v-flex>
+        <v-flex xs10 offset-xs1 sm6 offset-sm0 md4 v-for="album in albums" :key="album.id">
+          <photo :url="getThumbnail(album)">
+            <v-layout media column slot="card-media">
+              <v-spacer></v-spacer>
+              <v-card-title class="headline white--text">{{album.title}}</v-card-title>
+            </v-layout>
+
+            <v-layout row slot="card-actions">
+              <v-spacer></v-spacer>
+              <v-btn outline color="pink accent-1" :to="'/album/'+album.id">View Album</v-btn>
+            </v-layout>
+          </photo>
         </v-flex>
       </v-layout>
     </v-container>
@@ -15,6 +44,8 @@
   import Dashboard from '@/components/Dashboard'
   import Photo from '@/components/Photo'
 
+  let axios = require('axios')
+
   export default {
     name: "album",
     data() {
@@ -24,7 +55,6 @@
           backgroundImage: 'url(/static/media/img/bg2.jpg)',
           height: window.innerHeight +'px'
         },
-        src: "test",
         albums: []
       }
     },
@@ -32,21 +62,38 @@
       Dashboard,
       Photo
     },
-    created() {
-      this.$http.get('/api/album/')
-      .then(response => {
-        response.data.forEach(doc =>{
-          const data = {
-            'id': doc.pk,
-            'title': doc.title,
-            'description': doc.description,
-            'photos': doc.photos
-          }
-          this.albums.push(data)
+    beforeRouteEnter (to, from, next) {
+      axios.get('/api/album/')
+        .then(response => {
+          response.data.forEach(doc =>{
+            const data = {
+              'id': doc.pk,
+              'title': doc.title,
+              'photos': doc.photos
+            }
+            next(vm =>{
+              vm.albums.push(data)
+            })
+          })
         })
-      })
+    },
+    watch: {
+      '$route': 'fetchData'
     },
     methods: {
+      fetchData(){
+        this.$http.get('/api/album/')
+          .then(response => {
+            response.data.forEach(doc =>{
+              const data = {
+                'id': doc.pk,
+                'title': doc.title,
+                'photos': doc.photos
+              }
+              this.albums.push(data)
+            })
+          })
+      },
       getThumbnail(album) {
         if(album.photos[0]){
           const photo = album.photos[0]
@@ -55,11 +102,9 @@
           return null
         }
       }
-
     }
   }
 </script>
 
 <style scoped>
-
 </style>
