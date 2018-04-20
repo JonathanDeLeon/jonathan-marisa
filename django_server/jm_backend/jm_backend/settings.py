cba7314d54f3d435f16243cc44f606b8d02b7498
Jonathan De Leon <jonathan@campgroundautomation.com>
2018-04-20 15:04:08 -0700

    Set django server ready for production

--- django_server/jm_backend/jm_backend/settings.py
+++ django_server/jm_backend/jm_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'knhy%h006sceqj(k+sjx2*wpjz^a2c5)(@ndl4ey5@mvni&+t^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'jonathan-marisa.com', 'www.jonathan-marisa.com']
 
@@ -138,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/api/static/'
 
 # CORS Headers fix
 CORS_ORIGIN_ALLOW_ALL = True
