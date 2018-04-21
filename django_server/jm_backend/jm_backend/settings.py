54c196be9100c25ea9206d319bf90edd1d538c62
Jonathan De Leon <jonathan@campgroundautomation.com>
2018-04-20 17:09:08 -0700

    add SSL headers

--- django_server/jm_backend/jm_backend/settings.py
+++ django_server/jm_backend/jm_backend/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'jonathan-marisa.com', 'www.jonathan-marisa.com', 'api.jonathan-marisa.com']
 
+# Setup support for proxy headers
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 
