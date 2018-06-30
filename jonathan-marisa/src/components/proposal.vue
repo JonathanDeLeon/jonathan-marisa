b6d29a43d7835bcd9f11517bb8d84adbcd8e0d48
Jonathan De Leon <jonathan@campgroundautomation.com>
2018-06-29 19:16:26 -0700

    Add proposal component

--- jonathan-marisa/src/components/proposal.vue
+++ jonathan-marisa/src/components/proposal.vue
@@ -1,5 +1,23 @@
 <template>
-
+  <div id="proposal">
+    <v-container>
+      <v-layout wrap>
+        <v-flex xs12>
+          <p class="title text-xs-center">Jonathan & Marisa's</p>
+          <h3 class="display-header text-xs-center my-1">Proposal Story</h3>
+          <p class="title text-xs-center">December 8, 2017</p>
+          <p class="subheading text-xs-center"><em>As told by Marisa</em></p>
+        </v-flex>
+        <v-flex xs12>
+          <v-layout justify-center>
+            <v-flex xs12 md10>
+              <v-card-media height="700" contain src="/static/media/test.png"></v-card-media>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
